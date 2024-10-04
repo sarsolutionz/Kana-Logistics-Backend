016bd0ef0237e0e4f4bbde25b7af4ebf1f3ca8da
sarsolutionz <sarsolutionz7@gmail.com>
2024-10-04 12:35:29 +0530

    feat: Implement JWT Added

--- AdminApp/views.py
+++ AdminApp/views.py
@@ -2,10 +2,22 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 
+from rest_framework_simplejwt.tokens import RefreshToken
+
 from django.contrib.auth import authenticate
 
 from AdminApp.renderers import UserRenderer
 from AdminApp.serializers import UserSignUpSerializer, UserSignInSerializer
+
+# Creating tokens manually
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
 
 # Create your views here.
 
@@ -16,7 +28,8 @@ class UserSignUp(APIView):
         serializer = UserSignUpSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             user = serializer.save()
-            return Response({"msg": "User SignUp Successfully"}, status=status.HTTP_201_CREATED)
+            token = get_tokens_for_user(user=user)
+            return Response({"token": token, "msg": "User SignUp Successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -30,7 +43,8 @@ class UserSignIn(APIView):
             password = serializer.data.get("password")
             user = authenticate(email=email, password=password)
             if user is not None and user.is_admin:
-                return Response({"msg": "User SignIn Successfully"}, status=status.HTTP_200_OK)
+                token = get_tokens_for_user(user=user)
+                return Response({"token": token, "msg": "User SignIn Successfully"}, status=status.HTTP_200_OK)
             else:
                 return Response({"errors": {"non_field_errors": ["Email or Password is not valid"]}}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
